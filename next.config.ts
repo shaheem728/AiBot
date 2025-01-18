fea36870d80bb6014ac3a3699ee551cf5db8e3ce
shaheem mahmood m <140435351+shaheem728@users.noreply.github.com>
2025-01-18 20:56:15 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['127.0.0.1', 'localhost','shop-co-backend-rust.vercel.app'], 
+    domains: ['127.0.0.1', 'localhost','shop-co-backend-six.vercel.app'], 
   },
 };
 
