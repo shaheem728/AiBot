cb59f95d31c4236be21a18a4a5425c5f3f6f2a0a
shaheem mahmood m <140435351+shaheem728@users.noreply.github.com>
2025-01-18 20:57:20 +0530

    Update config.tsx

--- src/components/config.tsx
+++ src/components/config.tsx
@@ -1,2 +1,2 @@
-export const API_URL = process.env.REACT_APP_API_URL || 'https://shop-co-backend-rust.vercel.app';
-export const API_ADMIN = 'https://shop-co-backend-rust.vercel.app';
+export const API_URL = process.env.REACT_APP_API_URL || 'https://shop-co-backend-six.vercel.app';
+
