27fc0aa2a0de22d9ae0c0617f189823cd83df8b2
shaheem mahmood m <140435351+shaheem728@users.noreply.github.com>
2025-01-22 20:12:01 +0530

    Update config.tsx

--- src/components/config.tsx
+++ src/components/config.tsx
@@ -1,1 +1,2 @@
-export const API_URL = process.env.REACT_APP_API_URL || 'https://shop-co-backend-six.vercel.app';
+// export const API_URL = process.env.REACT_APP_API_URL || 'https://shop-co-backend-six.vercel.app';
+export const API_URL = 'https://shop-co-backend-six.vercel.app';
