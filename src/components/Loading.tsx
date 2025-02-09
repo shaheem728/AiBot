fd4052aff23f081faa33186de695bb780b2aa06c
shaheem mahmood m <140435351+shaheem728@users.noreply.github.com>
2025-02-09 12:26:02 +0530

    Update Loading.tsx

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,18 +1,1 @@
-import dynamic from 'next/dynamic';
-import React from 'react';
-function Loading() {
-  return (
-    <div className='flex items-center justify-center my-60'>
-        <div className="loader">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-    </div>
-   </div>
-  );
-}
-export default dynamic(()=>Promise.resolve(Loading),{ssr:false});
+
